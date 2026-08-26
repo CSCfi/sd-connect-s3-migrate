5885708511d1c52cbb3d04b95d5a461d32c9cc91
Monika Radaviciute <mradavic@csc.fi>
2026-08-26 11:44:58 +0300

    Update new version date

--- sd_connect_s3_migrate_gui/src/scripts/config.js
+++ sd_connect_s3_migrate_gui/src/scripts/config.js
@@ -16,4 +16,4 @@ export async function getSDConnectAPIEndpoint() {
   }
 }
 
-export const NEW_VERSION_DATE = Date.parse("2026-05-01T00:00:00Z");
+export const NEW_VERSION_DATE = Date.parse("2026-09-21T06:00:00Z");
